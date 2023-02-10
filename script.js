6631720494b14c85f8ddaf44306d444ce29ce352
nostalgiccat <nesdom27@gmail.com>
2023-02-09 20:14:38 -0700

    added javascript for api and async function

---
+++ script.js
@@ -0,0 +1,20 @@
+// Nasa API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+//Get 10 Images from Nasa API
+async function getNasaPictures() {
+    try {
+        const response = await fetch(apiUrl); 
+        resultsArray = await response.json(); 
+        console.log(resultsArray);
+    } catch (error) {
+        // Catch Error
+    }
+}
+
+// On Load
+getNasaPictures(); 
